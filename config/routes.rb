76ac40c730e32490b99840308029d2492b8468ce
Davi <davi.guimaraesleite@gmail.com>
2018-11-14 16:00:28 -0300

    figaro

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'product/new', to: "product#new"
-  get 'product', to: "product#index"
-  post 'product/new', to: "product#create"
+  get 'product/new', to: "product#new", as: 'new_product'
+  get 'products', to: "product#index", as: 'products'
+  post 'product/new', to: "product#create", as: 'create_product'
 
   root to: 'product#index'
 
